a9d4d7798e56f2034e968557b09fe1efdaff13de
Anssi Ylönen <anylonen@gmail.com>
2014-06-16 22:47:08 +0300

    Removed rangestrike and combat from defs.h

--- defs.h
+++ defs.h
@@ -708,32 +708,8 @@ for example. */
 #define M_SP_LAIR 241
 #define M_SP_PRIME 242
 
-/* rangestrike functions */
-#define M_STRIKE_MISSILE 301
-#define M_STRIKE_FBOLT 302
-#define M_STRIKE_LBALL 303
-#define M_STRIKE_FBALL 304
-#define M_STRIKE_BLIND 305
-#define M_STRIKE_SNOWBALL 306
-#define M_STRIKE_MASTER 307
-#define M_STRIKE_SONIC 308
-
-/* combat functions */
-#define M_MELEE_NORMAL 401
-#define M_MELEE_FIRE 402
-#define M_MELEE_DRAGON 403
-#define M_MELEE_MP 404
-#define M_MELEE_ELEC 405
-#define M_MELEE_POISON 406
-#define M_MELEE_NG 407
-#define M_MELEE_SUCCUBUS 408
-#define M_MELEE_SPIRIT 409
-#define M_MELEE_DISEASE 410
-#define M_MELEE_SLEEP 411
-#define M_MELEE_COLD 412
-#define M_MELEE_MASTER 413
-#define M_MELEE_GRAPPLE 414
-#define M_MELEE_DEATH 415
+/* rangestrike and combat functions */
+#include "combat.h"
 
 /* movement functions */
 #include "movement.h"
