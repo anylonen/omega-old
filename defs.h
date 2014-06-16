04699f2a6e768db033ab91992f32dae1fbc8d0f6
Anssi Ylönen <anylonen@gmail.com>
2014-06-16 18:41:25 +0300

    Removed environment from defs.h

--- defs.h
+++ defs.h
@@ -7,6 +7,7 @@ definitions in the following section. */
 
 /* Transferred typedefs */
 #include "symbol.h"
+#include "environment.h"
 
 #include "bank_account.h"
 #include "room.h"
@@ -299,35 +300,6 @@ on save and restore. */
 #define ATTACKED_ORACLE		0x1000000
 #define UNDEAD_GUARDS		0x2000000
     /* 26 so far... */
-
-/* non-existant environments for the random number seeding routine */
- /* added 12/30/98 (DG) */
-#define E_RESTORE -2
-#define E_RANDOM -1
-/* general environment types */
-#define E_NEVER_NEVER_LAND 0
-#define E_COUNTRYSIDE 1
-#define E_CITY 2
-#define E_VILLAGE 3
-#define E_TACTICAL_MAP 4
-#define E_SEWERS 5
-#define E_CASTLE 6
-#define E_CAVES 7
-#define E_VOLCANO 8
-#define E_ASTRAL 9
-#define E_ARENA 10
-#define E_HOVEL 11
-#define E_MANSION 12
-#define E_HOUSE 13
-#define E_DLAIR 14
-#define E_ABYSS 15
-#define E_STARPEAK 16
-#define E_MAGIC_ISLE 17
-#define E_TEMPLE 18
-#define E_CIRCLE 19
-#define E_COURT 20
-#define E_PALACE 21
-#define E_MAX E_PALACE
 
 /* player game status */
 #define DEAD 1
---
+++ environment.h
@@ -0,0 +1,33 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+/* non-existant environments for the random number seeding routine */
+ /* added 12/30/98 (DG) */
+#define E_RESTORE -2
+#define E_RANDOM -1
+/* general environment types */
+#define E_NEVER_NEVER_LAND 0
+#define E_COUNTRYSIDE 1
+#define E_CITY 2
+#define E_VILLAGE 3
+#define E_TACTICAL_MAP 4
+#define E_SEWERS 5
+#define E_CASTLE 6
+#define E_CAVES 7
+#define E_VOLCANO 8
+#define E_ASTRAL 9
+#define E_ARENA 10
+#define E_HOVEL 11
+#define E_MANSION 12
+#define E_HOUSE 13
+#define E_DLAIR 14
+#define E_ABYSS 15
+#define E_STARPEAK 16
+#define E_MAGIC_ISLE 17
+#define E_TEMPLE 18
+#define E_CIRCLE 19
+#define E_COURT 20
+#define E_PALACE 21
+#define E_MAX E_PALACE
+
+#endif
