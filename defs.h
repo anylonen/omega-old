dfcb6abd6955ef3f76cacd48d7af2c1d91e9c65b
Anssi Ylönen <anylonen@gmail.com>
2014-06-16 20:47:03 +0300

    Removed traps from defs.h

--- defs.h
+++ defs.h
@@ -1334,27 +1334,7 @@ for example. */
 #define LEVITATION_AVOIDANCE 100
 
 /* traps */
-#define NUMTRAPS 13
-#define TRAP_BASE 101
-
-#define L_TRAP_DART 101
-#define L_TRAP_PIT 102
-#define L_TRAP_DOOR 103
-#define L_TRAP_SNARE 104
-#define L_TRAP_BLADE 105
-#define L_TRAP_FIRE 106
-#define L_TRAP_TELEPORT 107
-#define L_TRAP_DISINTEGRATE 108
-#define L_TRAP_SLEEP_GAS 109
-#define L_TRAP_ACID 110
-#define L_TRAP_MANADRAIN 111
-#define L_TRAP_ABYSS 112
-#define L_TRAP_SIREN 113
-
-#define NUM_SAFE_TRAPS 7
-/* the first NUM_SAFE_TRAPS are non-insta-lethal */
-
-/* leave head room for extra traps: 114, 115, 116, 117, 118, 119, 120 */
+#include "traps.h"
 
 /* random sites */
 #include "sites.h"
