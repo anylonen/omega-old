d13599ddd921bb57857780dc0185346fa8b3cf00
Anssi Ylönen <anylonen@gmail.com>
2014-06-16 20:39:42 +0300

    Removed player possession slots from defs.h

---
+++ possession.h
@@ -0,0 +1,24 @@
+#ifndef POSSESSION_H
+#define POSSESSION_H
+
+/* player possession slots */
+/* slot 0 should not be filled when out of inventory_control() */
+
+#define O_UP_IN_AIR 0
+#define O_READY_HAND 1
+#define O_WEAPON_HAND 2
+#define O_LEFT_SHOULDER 3
+#define O_RIGHT_SHOULDER 4
+#define O_BELT1 5
+#define O_BELT2 6
+#define O_BELT3 7
+#define O_SHIELD 8 
+#define O_ARMOR 9
+#define O_BOOTS 10
+#define O_CLOAK 11
+#define O_RING1 12
+#define O_RING2 13
+#define O_RING3 14
+#define O_RING4 15
+
+#endif
