c2fec164943422b95e82633ca5f841c9a970391a
Anssi Ylönen <anylonen@gmail.com>
2014-07-04 16:05:59 +0300

    Refactored priest.[hc]
    Mostly extracted methods from the increase_priest_rank method.

--- src/priest.h
+++ src/priest.h
@@ -12,5 +12,13 @@ void hp_req_test( void );
 int increase_priest_rank( int );
 void l_altar( void );
 void make_hp( pob );
-
+void player_join_patron(const int patron);
+void can_player_join_odin(void);
+void can_player_join_set(void);
+void can_player_join_athena(void);
+void can_player_join_hecate(void);
+void can_player_join_druid(void);
+void can_player_join_destiny(void);
+void player_has_no_priesthood_rank(const int deity);
+const int player_has_priesthood_rank(const int deity);
 #endif
