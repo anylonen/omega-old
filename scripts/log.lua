cd7d5346622f702d71a97c9fc9e601f3695c8612
Anssi Ylönen <anylonen@gmail.com>
2014-08-15 15:38:12 +0300

    Added log.lua for logging

---
+++ scripts/log.lua
@@ -0,0 +1,10 @@
+log_file = "debug_log.txt"
+
+function datetime()
+    return os.date("%Y.%m.%d %H:%M:%S")
+end
+
+function write_log(message)
+    file = io.open(log_file, "a")
+    file:write(datetime() .. ": " .. message .. "\n")
+end
